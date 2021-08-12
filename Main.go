fd2b5eac5e1bf0d9885fcdf17c56b7b8e1953eb7
xhuanya <xhuan_blog@yeah.net>
2021-08-12 09:22:11 +0800

    用户配置文件改为参数传入 增加任务执行时间

--- Main.go
+++ Main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"os"
+	"time"
 )
 
 func main() {
-	byte, err := os.ReadFile("./user.yaml")
+
+	var path string
+	flag.StringVar(&path, "file", "", "账户配置")
+	flag.Parse()
+	if len(path)==0 {
+		fmt.Printf("没有配置账户文件！")
+		os.Exit(1)
+	}
+	byte, err := os.ReadFile(path)
 	if err != nil {
 		fmt.Printf("打开用户信息失败%s", err.Error())
 	}
+	fmt.Printf("==================%s================\r\n",time.Now())
+	fmt.Printf("续期任务开始拉~")
+	
 	var user []User
 	yaml.Unmarshal(byte, &user)
 	for item := range user {
